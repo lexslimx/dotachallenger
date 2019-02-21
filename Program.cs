825ff1a9bab3e7843e19ae16c17dabf6d3c1fe21
Wamwea <Mynameiswamwea@gmail.com>
2019-02-21 22:29:17 +0300

    Created Game Class and constructor

--- Program.cs
+++ Program.cs
@@ -2,15 +2,35 @@
 
 namespace dotachallenger
 {
+     class Game
+    {  
+       
+        public Game(int killcount, int deaths, int assists)
+        {
+        this.killcount=killcount;
+        this.deaths=deaths;
+        this.assists=assists;
+        }
+        
+       public int killcount;
+       public int deaths;
+       public int assists;
+        DateTime gametime;
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Dota!");                        
-            //Console.WriteLine("Please enter your steam account id");
+            Console.WriteLine("Please enter your steam account id");
             string accountId = null;
+           
+            Game FirstGame=new Game(5,3,8);
+            Console.WriteLine($"Your KDA is {FirstGame.killcount}:{FirstGame.deaths}:{FirstGame.assists}");
+            
             //read the accountId from the console (**hint user Console.ReadLine) to get rid of the compile error
-
+         
             //Console.WriteLine($"You Entered: {accountId}");
             
              // TODO
